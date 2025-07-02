445638c523d6a01adf967388fcd3d196120a9880
CODE WEB <gabrielronaldo9343@gmail.com>
2025-07-02 18:59:21 -0400

    CRUD Scaffolding

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -1,18 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
-using System.Collections.Generic;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc.Controllers
 {
     public class DepartmentsController : Controller
     {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentsController(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            List<Department> list = new List<Department>();
-            list.Add(new Department { Id = 1, Name = "Eletronics" });
-            list.Add(new Department { Id = 2, Name = "Fashion" });
-
-            return View(list);
+            return View();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,9 @@ namespace SalesWebMvc {
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
